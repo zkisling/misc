446218c178f745f25cead72aaabe2171de38fa7d
Zack Kisling <zkisling@businessinsider.com>
2013-02-26 13:20:33 -0500

    Removing validate function

--- linkedlist.php
+++ linkedlist.php
@@ -24,6 +24,11 @@ class LinkedList
 {
 	protected $head = null;
 
+	public function isEmpty()
+	{
+		return $this->head == null;
+	}
+
 	public function z_push($data)
 	{
 		$this->head = new Node($data, $this->head);
@@ -32,25 +37,18 @@ class LinkedList
 	public function z_pop()
 	{
 		if ($this->head) {
-			$value = $this->head;
+			$value = $this->head->data;
 			$this->head = $this->head->next;
 			return $value;
 		}
 		return null;
 	}
+}
 
-	public function printz()
-	{
-		$current = $this->head;
-		while ($current) {
-			echo $current->data, "\n";
-			$current = $current->next;
-		}
-	}
-
-	public function validate($str)
-	{
-		$length = strlen($str);
+// Test me
+$ll = new LinkedList();
+$str = '[]{}(){';
+$length = strlen($str);
 		for ($i = 0; $i < $length; $i++) {
 			$p = $str[$i];
 			// If string starts with open brackets then push
@@ -59,7 +57,7 @@ class LinkedList
 				case "(":
 				case "{":
 				case "<":
-				$this->z_push($str[$i]);
+				$ll->z_push($str[$i]);
 				break;
 			// If string starts with closed brackets then pop
 				case "]":
@@ -67,9 +65,8 @@ class LinkedList
 				case "}":
 				case ">":
 					// set popped node data to $chr
-					if ($c = $this->z_pop()) {
-						$chr = $c->data;
-					}
+					$chr = $ll->z_pop();
+
 					if (!($chr == '[' && $p == ']') &&
 						!($chr == '(' && $p == ')') &&
 						!($chr == '{' && $p == '}') &&
@@ -82,19 +79,11 @@ class LinkedList
 				}
 			}
 
-		if ($this->head) {
+		if (! $ll->isEmpty()) {
 			echo "Wrong, there is a node left\n";
 		} else {
 			echo "Success\n";
 		}
-	}
-}
-// Test me
-$ll = new LinkedList();
-$str = '[]{}()';
-$ll->validate($str);
-while ($node = $ll->z_pop()) {
-	echo "Remaining node data : " . $node->data . PHP_EOL;
-}
+
 
 
